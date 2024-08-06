01f0d4be736be66fa8f72bb64cf09c41e4030c50
Kirill Korotkov <kkorotkov@owner.one>
2024-08-06 16:20:02 +0200

    feat: impl navigation with keyboard

--- listbox/src/lib.rs
+++ listbox/src/lib.rs
@@ -1,7 +1,7 @@
 use std::rc::Rc;
 
 use presence::*;
-use utils::hooks::use_controllable_state::use_controllable_state;
+use utils::hooks::{use_children_as_html_collection, use_controllable_state, use_keydown};
 use yew::prelude::*;
 
 #[derive(Clone, PartialEq, Debug)]
@@ -17,21 +17,15 @@ impl Default for ListboxSelected {
 }
 
 #[derive(Clone, PartialEq, Debug)]
-pub enum ListboxSelectedAction {
-    Select(AttrValue),
-    Deselect(AttrValue),
-}
-
-#[derive(Clone, PartialEq, Debug)]
 pub struct ListboxContext {
+    pub active: AttrValue,
     pub selected: ListboxSelected,
-    pub on_selected_change: Callback<ListboxSelectedAction>,
+    pub on_selected_change: Callback<ListboxSelected>,
 }
 
 pub enum ListboxAction {
-    Select(AttrValue),
-    Deselect(AttrValue),
-    SelectAll,
+    ToggleSelection(AttrValue),
+    SetActive(AttrValue),
 }
 
 impl Reducible for ListboxContext {
@@ -39,45 +33,45 @@ impl Reducible for ListboxContext {
 
     fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
         match action {
-            ListboxAction::Select(id) => {
+            ListboxAction::ToggleSelection(id) => {
                 let selected = match self.selected.clone() {
-                    ListboxSelected::Single(_) => ListboxSelected::Single(Some(id.clone())),
+                    ListboxSelected::Single(prev) => match prev {
+                        Some(prev) => {
+                            if prev == id {
+                                ListboxSelected::Single(None)
+                            } else {
+                                ListboxSelected::Single(Some(id.clone()))
+                            }
+                        }
+                        None => ListboxSelected::Single(Some(id.clone())),
+                    },
                     ListboxSelected::Multiple(prev) => {
-                        let mut next = prev.clone();
-                        next.push(id.clone());
-                        ListboxSelected::Multiple(next)
+                        if prev.contains(&id) {
+                            let mut next = prev.clone();
+                            next.retain(|v| v != &id);
+                            ListboxSelected::Multiple(next)
+                        } else {
+                            let mut next = prev.clone();
+                            next.push(id.clone());
+                            ListboxSelected::Multiple(next)
+                        }
                     }
                 };
 
-                self.on_selected_change
-                    .emit(ListboxSelectedAction::Select(id));
+                self.on_selected_change.emit(selected.clone());
 
                 ListboxContext {
                     selected,
+                    active: id,
                     ..(*self).clone()
                 }
                 .into()
             }
-            ListboxAction::Deselect(id) => {
-                let selected = match self.selected.clone() {
-                    ListboxSelected::Single(_) => ListboxSelected::Single(None),
-                    ListboxSelected::Multiple(prev) => {
-                        let mut next = prev.clone();
-                        next.retain(|v| v != &id);
-                        ListboxSelected::Multiple(next)
-                    }
-                };
-
-                self.on_selected_change
-                    .emit(ListboxSelectedAction::Deselect(id));
-
-                ListboxContext {
-                    selected,
-                    ..(*self).clone()
-                }
-                .into()
+            ListboxAction::SetActive(id) => ListboxContext {
+                active: id,
+                ..(*self).clone()
             }
-            ListboxAction::SelectAll => self,
+            .into(),
         }
     }
 }
@@ -131,38 +125,101 @@ pub fn listbox(props: &ListboxProps) -> Html {
         props.on_selected_change.clone(),
     );
 
-    let on_selected_change = use_callback(
-        dispatch.clone(),
-        |selected: ListboxSelectedAction, dispatch| {
-            dispatch.emit(Box::new(move |state| match state {
-                ListboxSelected::Single(_) => match selected.clone() {
-                    ListboxSelectedAction::Select(value) => ListboxSelected::Single(Some(value)),
-                    ListboxSelectedAction::Deselect(_) => ListboxSelected::Single(None),
-                },
-                ListboxSelected::Multiple(prev) => match selected.clone() {
-                    ListboxSelectedAction::Select(id) => {
-                        let mut next = prev.clone();
-                        next.push(id);
-                        ListboxSelected::Multiple(next)
-                    }
-                    ListboxSelectedAction::Deselect(id) => {
-                        let mut next = prev.clone();
-                        next.retain(|v| v != &id);
-                        ListboxSelected::Multiple(next)
-                    }
-                },
-            }));
-        },
-    );
+    let on_selected_change =
+        use_callback(dispatch.clone(), |selected: ListboxSelected, dispatch| {
+            dispatch.emit(Box::new(move |_| selected.clone()));
+        });
 
     let context_value = use_reducer(|| ListboxContext {
+        active: AttrValue::from(""),
         selected: (*selected).clone().borrow().clone(),
         on_selected_change,
     });
 
+    let node_ref = use_node_ref();
+    let options = use_children_as_html_collection(node_ref.clone());
+
+    let navigation_handler = {
+        let options = options.clone();
+        let context_value = context_value.clone();
+
+        move |event: KeyboardEvent| {
+            let options = options.borrow();
+            let options = options.as_ref();
+            let active = context_value.active.clone();
+
+            if event.key() == " " {
+                context_value.dispatch(ListboxAction::ToggleSelection(AttrValue::from(active)));
+                return;
+            }
+
+            if options.is_none() {
+                return;
+            }
+
+            let options = options.unwrap();
+
+            let current_option_index = if active.is_empty() {
+                None
+            } else {
+                let mut index = None;
+
+                for i in 0..options.length() {
+                    if let Some(option) = options.item(i) {
+                        if option.id() == active {
+                            index = Some(i);
+                            break;
+                        }
+                    }
+                }
+
+                index
+            };
+
+            let next_index = {
+                if let Some(index) = current_option_index {
+                    match event.key().as_str() {
+                        "ArrowDown" => {
+                            if index + 1 >= options.length() {
+                                Some(0)
+                            } else {
+                                Some(index + 1)
+                            }
+                        }
+                        "ArrowUp" => {
+                            if index == 0 {
+                                Some(options.length() - 1)
+                            } else {
+                                Some(index - 1)
+                            }
+                        }
+                        _ => None,
+                    }
+                } else {
+                    match event.key().as_str() {
+                        "ArrowDown" => Some(0),
+                        "ArrowUp" => Some(options.length() - 1),
+                        _ => None,
+                    }
+                }
+            };
+
+            if let Some(index) = next_index {
+                if let Some(option) = options.item(index) {
+                    context_value.dispatch(ListboxAction::SetActive(AttrValue::from(option.id())));
+                }
+            }
+        }
+    };
+
+    let navigate_through_options = use_keydown(
+        vec!["ArrowDown".into(), "ArrowUp".into(), " ".into()],
+        navigation_handler,
+    );
+
     html! {
-        <ContextProvider<MutableListboxContext> context={context_value}>
-            <ul role="listbox" tabindex="0" class={props.class.clone()} aria-multiselectable={if props.multiple { "true" } else { "false" }}>
+        <ContextProvider<MutableListboxContext> context={context_value.clone()}>
+            <ul ref={node_ref} role="listbox" tabindex="0" class={props.class.clone()} aria-multiselectable={if props.multiple { "true" } else { "false" }} aria-activedescendant={context_value.active.clone()} onkeydown={navigate_through_options}>
                 {for props.children.iter()}
             </ul>
         </ContextProvider<MutableListboxContext>>
@@ -212,32 +269,49 @@ pub fn listbox_option(props: &ListboxOptionProps) -> Html {
     });
 
     let select_on_click = use_callback(
-        (props.id, is_selected.clone(), context.clone()),
-        |_event: MouseEvent, (id, is_selected, context)| {
-            if **is_selected {
-                context.dispatch(ListboxAction::Deselect(AttrValue::from(*id)));
-            } else {
-                context.dispatch(ListboxAction::Select(AttrValue::from(*id)));
+        (props.id, context.clone()),
+        |_event: MouseEvent, (id, context)| {
+            context.dispatch(ListboxAction::ToggleSelection(AttrValue::from(*id)));
+        },
+    );
+
+    use_effect_with(
+        (context.clone(), is_selected.clone(), props.id),
+        |(context, is_selected, id)| {
+            if let ListboxSelected::Single(selected) = &context.selected {
+                match selected {
+                    Some(selected) => {
+                        if selected == id && !**is_selected {
+                            context.dispatch(ListboxAction::ToggleSelection(AttrValue::from(*id)));
+                        }
+                    }
+                    None => {
+                        if !**is_selected {
+                            context.dispatch(ListboxAction::ToggleSelection(AttrValue::from(*id)));
+                        }
+                    }
+                }
             }
         },
     );
 
-    let select_on_focus = use_callback(
-        (props.id, context.clone()),
-        |_event: FocusEvent, (id, context)| {
-            context.dispatch(ListboxAction::Select(AttrValue::from(*id)));
-        },
-    );
+    let data_active = use_memo(context.active.clone(), |active| {
+        if *active == props.id {
+            "true"
+        } else {
+            "false"
+        }
+    });
 
     let element = if *is_multiple {
         html! {
-            <li id={props.id} role="option" class={props.class.clone()} aria-checked={*aria_selected} onclick={select_on_click} onfocus={select_on_focus}>
+            <li id={props.id} role="option" class={props.class.clone()} aria-checked={*aria_selected} data-active={*data_active} onclick={select_on_click}>
                 {for props.children.iter()}
             </li>
         }
     } else {
         html! {
-            <li id={props.id} role="option" class={props.class.clone()} aria-selected={*aria_selected} onclick={select_on_click}>
+            <li id={props.id} role="option" class={props.class.clone()} aria-selected={*aria_selected} data-active={*data_active} onclick={select_on_click}>
                 {for props.children.iter()}
             </li>
         }
