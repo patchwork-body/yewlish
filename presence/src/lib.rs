6a10fde90880dd57adba8114d394f1c15cc9db1c
Kirill Korotkov <kkorotkov@owner.one>
2024-08-06 10:59:57 +0200

    refactor: Refactor use_presence hook to accept NodeRef as an argument

--- presence/src/lib.rs
+++ presence/src/lib.rs
@@ -5,24 +5,58 @@ use yew::prelude::*;
 mod hooks;
 
 #[derive(Debug, Clone, PartialEq, Properties)]
+pub struct PresenceRenderAsProps {
+    pub presence: bool,
+    pub r#ref: NodeRef,
+    #[prop_or_default]
+    pub children: Children,
+    #[prop_or_default]
+    pub class: Option<AttrValue>,
+}
+
+#[derive(Debug, Clone, PartialEq, Properties)]
 pub struct PresenceProps {
+    #[prop_or_default]
+    pub name: &'static str,
     #[prop_or_default]
     pub children: Children,
     #[prop_or_default]
     pub present: bool,
     #[prop_or_default]
     pub class: Option<AttrValue>,
+    #[prop_or_default]
+    pub render_as: Option<Callback<PresenceRenderAsProps, Html>>,
 }
 
 #[function_component(Presence)]
 pub fn presence(props: &PresenceProps) -> Html {
-    let (presence, node_ref) = use_presence(props.present);
+    let node_ref = use_node_ref();
+    let presence = use_presence(props.present, node_ref.clone());
 
-    html! {
-        <AttrReceiver>
-            <div hidden={!*presence} ref={node_ref} class={&props.class}>
+    let element = if let Some(render_as) = &props.render_as {
+        html! {{
+            render_as.emit(PresenceRenderAsProps {
+                presence: *presence,
+                r#ref: node_ref.clone(),
+                children: props.children.clone(),
+                class: props.class.clone(),
+            })
+        }}
+    } else {
+        if !*presence {
+            return html! {};
+        }
+
+        html! {
+            <div ref={node_ref} class={props.class.clone()}>
                 {props.children.clone()}
             </div>
+        }
+    };
+
+    html! {
+        <AttrReceiver name={props.name}>
+            {element}
         </AttrReceiver>
     }
 }
