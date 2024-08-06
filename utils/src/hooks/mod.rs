b622cabde54380884d2e8e6e70b9f88f833fbd89
Kirill Korotkov <kkorotkov@owner.one>
2024-08-06 16:18:38 +0200

    refactor: Reorder import statements in hooks/mod.rs

--- utils/src/hooks/mod.rs
+++ utils/src/hooks/mod.rs
@@ -1,7 +1,9 @@
-pub mod use_interaction_outside;
-pub mod use_controllable_state;
-pub mod use_keydown;
+mod use_children_as_html_collection;
+mod use_controllable_state;
+mod use_interaction_outside;
+mod use_keydown;
 
-pub use use_interaction_outside::use_interaction_outside;
+pub use use_children_as_html_collection::use_children_as_html_collection;
 pub use use_controllable_state::use_controllable_state;
+pub use use_interaction_outside::use_interaction_outside;
 pub use use_keydown::use_keydown;
