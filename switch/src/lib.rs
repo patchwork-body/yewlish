e1cc69e354a54d28614753be02a11000d05f73bb
patchwork-body <personal.gugfug@gmail.com>
2024-09-21 18:00:45 +0200

    refactor(switch): update switch component to support readonly state and custom rendering

--- switch/src/lib.rs
+++ switch/src/lib.rs
@@ -1,10 +1,43 @@
 use std::rc::Rc;
 
 use yew::prelude::*;
-use yewlish_utils::{helpers::combine_handlers::combine_handlers, hooks::use_controllable_state};
+use yewlish_attr_passer::{attributify, AttrPasser, AttrReceiver};
+use yewlish_utils::{
+    helpers::combine_handlers::combine_handlers,
+    hooks::{use_conditional_attr, use_controllable_state},
+};
+
+#[derive(Clone, Debug, PartialEq, Properties)]
+pub struct SwitchRenderAsProps {
+    #[prop_or_default]
+    pub r#ref: NodeRef,
+    #[prop_or_default]
+    pub children: ChildrenWithProps<SwitchThumb>,
+    #[prop_or_default]
+    pub id: Option<AttrValue>,
+    #[prop_or_default]
+    pub class: Option<AttrValue>,
+    #[prop_or_default]
+    pub checked: bool,
+    #[prop_or_default]
+    pub disabled: bool,
+    #[prop_or_default]
+    pub required: bool,
+    #[prop_or_default]
+    pub name: Option<AttrValue>,
+    #[prop_or_default]
+    pub value: Option<AttrValue>,
+    #[prop_or_default]
+    pub readonly: bool,
+    #[prop_or_default]
+    pub toggle: Callback<()>,
+}
 
 #[derive(Clone, Debug, PartialEq, Properties)]
 pub struct SwitchProps {
+    #[prop_or_default]
+    pub r#ref: NodeRef,
+    #[prop_or_default]
     pub children: ChildrenWithProps<SwitchThumb>,
     #[prop_or_default]
     pub id: Option<AttrValue>,
@@ -26,6 +59,10 @@ pub struct SwitchProps {
     pub value: Option<AttrValue>,
     #[prop_or_default]
     pub onclick: Option<Callback<MouseEvent>>,
+    #[prop_or_default]
+    pub readonly: bool,
+    #[prop_or_default]
+    pub render_as: Option<Callback<SwitchRenderAsProps, Html>>,
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -77,31 +114,66 @@ pub fn switch(props: &SwitchProps) -> Html {
     );
 
     let toggle = use_callback(
-        context_value.clone(),
-        move |_event: MouseEvent, context_value| {
+        (dispatch.clone(), context_value.clone(), props.readonly),
+        move |(), (dispatch, context_value, readonly)| {
+            if *readonly {
+                return;
+            }
+
             dispatch.emit(Box::new(|prev_state| !prev_state));
             context_value.dispatch(SwitchAction::Toggle);
         },
     );
 
+    let toggle_on_click = use_callback(toggle.clone(), move |_: MouseEvent, toggle| {
+        toggle.emit(());
+    });
+
+    use_conditional_attr(props.r#ref.clone(), "data-disabled", props.disabled);
+
+    let element = if let Some(render_as) = &props.render_as {
+        render_as.emit(SwitchRenderAsProps {
+            r#ref: props.r#ref.clone(),
+            children: props.children.clone(),
+            id: props.id.clone(),
+            class: props.class.clone(),
+            checked: *checked.borrow(),
+            disabled: props.disabled,
+            required: props.required,
+            name: props.name.clone(),
+            value: props.value.clone(),
+            readonly: props.readonly,
+            toggle: toggle.clone(),
+        })
+    } else {
+        html! {
+            <AttrReceiver name="switch">
+                <button
+                    id={&props.id}
+                    class={&props.class}
+                    type="button"
+                    role="switch"
+                    disabled={props.disabled}
+                    name={&props.name}
+                    value={&props.value}
+                    onclick={&combine_handlers(props.onclick.clone(), toggle_on_click.into())}
+                >
+                    {for props.children.iter()}
+                </button>
+            </AttrReceiver>
+        }
+    };
+
     html! {
         <ContextProvider<ReducibleSwitchContext> context={context_value}>
-            <button
-                id={&props.id}
-                class={&props.class}
-                type="button"
-                role="switch"
-                aria-checked={checked.borrow().to_string()}
-                aria-required={props.required.then_some("true")}
-                data-state={if *checked.borrow() { "checked" } else { "unchecked" }}
-                data-disabled={props.disabled.to_string()}
-                disabled={props.disabled}
-                name={&props.name}
-                value={&props.value}
-                onclick={&combine_handlers(props.onclick.clone(), toggle.into())}
-            >
-                {for props.children.iter()}
-            </button>
+            <AttrPasser name="switch" ..attributify! {
+                "aria-checked" => checked.borrow().to_string(),
+                "aria-required" => props.required.then_some("true").unwrap_or_default(),
+                "data-state" => if *checked.borrow() { "checked" } else { "unchecked" },
+                "data-disabled" => props.disabled.to_string(),
+            }>
+                {element}
+            </AttrPasser>
         </ContextProvider<ReducibleSwitchContext>>
     }
 }
