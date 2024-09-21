317218166ca46a02f33e02f1c80d13c4f18b31f4
patchwork-body <personal.gugfug@gmail.com>
2024-09-21 19:50:29 +0200

    feat(enums): add IntoPropValue implementation for Dir enum

--- utils/src/enums/dir.rs
+++ utils/src/enums/dir.rs
@@ -1,5 +1,16 @@
+use yew::{html::IntoPropValue, AttrValue};
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Dir {
     Ltr,
     Rtl,
+}
+
+impl IntoPropValue<Option<AttrValue>> for Dir {
+    fn into_prop_value(self) -> Option<AttrValue> {
+        match self {
+            Dir::Ltr => Some("ltr".into()),
+            Dir::Rtl => Some("rtl".into()),
+        }
+    }
 }
