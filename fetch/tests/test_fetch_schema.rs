9fda14bc64abf53098a5c1e6533581cae1c954e0
patchwork-body <personal.gugfug@gmail.com>
2024-10-10 21:29:23 +0200

    test: update hook test case

--- fetch/tests/test_fetch_schema.rs
+++ fetch/tests/test_fetch_schema.rs
@@ -1,9 +1,12 @@
+use schema::{
+    use_todos_with_options, CreateTodoParams, DeleteTodoParams, TestFetchClient,
+    TestFetchClientProvider, TodoParams, TodosOptions, TodosParams, UpdateTodoParams,
+};
 use serde::{Deserialize, Serialize};
 use serial_test::serial;
 use std::{rc::Rc, sync::Arc};
 use wasm_bindgen_test::wasm_bindgen_test;
 use yew::prelude::*;
-use yewlish_fetch::FetchSchema;
 use yewlish_fetch_utils::*;
 use yewlish_testing_tools::*;
 
@@ -38,19 +41,25 @@ struct TodoSlug {
     id: u32,
 }
 
-#[derive(FetchSchema)]
-enum Test {
-    #[get("/todos/{id}", slugs = TodoSlug, res = Todo)]
-    Todo,
-    #[get("/todos", query = TodosQuery, res = Vec<Todo>)]
-    Todos,
-    #[post("/todos", body = CreateTodo, res = Todo)]
-    CreateTodo,
-    #[put("/todos/{id}", slugs = TodoSlug, body = UpdateTodo, res = Todo)]
-    UpdateTodo,
-    #[delete("/todos/{id}", slugs = TodoSlug, res = String)]
-    DeleteTodo,
+mod schema {
+    use yewlish_fetch::FetchSchema;
+
+    #[derive(FetchSchema)]
+    pub enum Test {
+        #[get("/todos/{id}", slugs = TodoSlug, res = Todo)]
+        Todo,
+        #[get("/todos", query = TodosQuery, res = Vec<Todo>)]
+        Todos,
+        #[post("/todos", body = CreateTodo, res = Todo)]
+        CreateTodo,
+        #[put("/todos/{id}", slugs = TodoSlug, body = UpdateTodo, res = Todo)]
+        UpdateTodo,
+        #[delete("/todos/{id}", slugs = TodoSlug, res = String)]
+        DeleteTodo,
+    }
 }
+
+pub use schema::Test;
 
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
@@ -207,4 +216,21 @@ async fn test_fetch_schema_client() {
 
     let result: Vec<Todo> = deserialize_response(result.unwrap().as_str()).unwrap();
     assert_eq!(result.len(), 0);
+}
+
+#[wasm_bindgen_test]
+#[serial]
+pub async fn test_hooks() {
+    let t = render!(
+        {
+            let query = use_todos_with_options(TodosParams::default(), TodosOptions::default());
+            use_remember_value(query.data);
+
+            html! {}
+        },
+        TestRoot
+    )
+    .await;
+
+    assert_eq!(t.get_state::<Vec<Todo>>().len(), 0);
 }
