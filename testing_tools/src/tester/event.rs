828b56885f3aabe26cd844d083fe0f9985f194d9
patchwork-body <personal.gugfug@gmail.com>
2024-10-13 19:56:18 +0200

    feat: simulate focus event

--- testing_tools/src/tester/event.rs
+++ testing_tools/src/tester/event.rs
@@ -3,4 +3,5 @@ use std::{future::Future, pin::Pin};
 pub trait TesterEvent {
     fn click(self) -> Pin<Box<dyn Future<Output = Self>>>;
     fn keydown(self, key: &str) -> Pin<Box<dyn Future<Output = Self>>>;
+    fn focus(self) -> Pin<Box<dyn Future<Output = Self>>>;
 }
