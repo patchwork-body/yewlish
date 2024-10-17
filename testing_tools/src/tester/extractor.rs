f3e8a9c145aab8c5062745257059fd7cd6b0fd66
patchwork-body <personal.gugfug@gmail.com>
2024-10-17 13:07:16 +0200

    chore(testing-tools): Update crate version to 1.2.1 and deprecate get_state method

--- testing_tools/src/tester/extractor.rs
+++ testing_tools/src/tester/extractor.rs
@@ -1,5 +1,7 @@
 pub trait Extractor {
     fn attribute(&self, name: &str) -> Option<String>;
     fn text(&self) -> String;
+    #[deprecated(since = "1.2.1", note = "Please use `get_remembered_value` instead")]
     fn get_state<T: Clone + 'static>(&self) -> T;
+    fn get_remembered_value<T: Clone + 'static>(&self) -> T;
 }
