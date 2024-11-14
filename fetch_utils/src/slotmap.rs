8d2d95a5692977b8ba6868b19acec647bf132c38
patchwork-body <personal.gugfug@gmail.com>
2024-11-14 21:17:50 +0100

    feat: signals for the queries

--- fetch_utils/src/slotmap.rs
+++ fetch_utils/src/slotmap.rs
@@ -44,4 +44,39 @@ impl<T> SlotMap<T> {
     pub fn is_empty(&self) -> bool {
         self.items.is_empty()
     }
+
+    pub fn iter(&self) -> impl Iterator<Item = (&usize, &T)> {
+        self.items.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = (&usize, &mut T)> {
+        self.items.iter_mut()
+    }
+}
+
+impl<T> IntoIterator for SlotMap<T> {
+    type Item = (usize, T);
+    type IntoIter = std::collections::hash_map::IntoIter<usize, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.items.into_iter()
+    }
+}
+
+impl<'a, T> IntoIterator for &'a SlotMap<T> {
+    type Item = (&'a usize, &'a T);
+    type IntoIter = std::collections::hash_map::Iter<'a, usize, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.items.iter()
+    }
+}
+
+impl<'a, T> IntoIterator for &'a mut SlotMap<T> {
+    type Item = (&'a usize, &'a mut T);
+    type IntoIter = std::collections::hash_map::IterMut<'a, usize, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.items.iter_mut()
+    }
 }
