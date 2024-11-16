2998f3e926f85c99f07334d21889d9339a8fb710
patchwork-body <personal.gugfug@gmail.com>
2024-11-16 21:49:38 +0100

    chore(fetch): Bump version to 0.3.0-beta.18 and enhance logging for query keys in fetch_schema function

--- fetch/src/lib.rs
+++ fetch/src/lib.rs
@@ -341,7 +341,8 @@ pub fn fetch_schema(input: TokenStream) -> TokenStream {
 
                             web_sys::console::log_1(&format!("Updating queries for {state_key}").into());
                             web_sys::console::log_1(&format!("Queries: {queries:?}").into());
-                            web_sys::console::log_1(&format!("Queries keys: {:?}", queries.get_mut(&state_key)).into());
+                            web_sys::console::log_1(&format!("Queries: {:?}", queries.keys()).into());
+                            web_sys::console::log_1(&format!("Queries: {:?}", queries.get_mut(&state_key)).into());
 
                             if let Some(slotmap) = queries.get_mut(&state_key) {
                                 web_sys::console::log_1(&format!("Updating queries for {state_key}").into());
